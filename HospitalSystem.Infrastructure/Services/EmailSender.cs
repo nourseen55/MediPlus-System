9ed64a0e3418b06818690ffe55b8993bb7377db5
Hussein Elghareb <halghryb287@gmail.com>
2024-09-24 21:49:10 +0300

    Email sender service was created

---
+++ HospitalSystem.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalSystem.Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}

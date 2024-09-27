c8adab4b414476890998fbfcf69ed74a69b99c7d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-09-27 04:35:36 +0300

    push Appoinments

--- Hospital Management Project/globalUsings.cs
+++ Hospital Management Project/globalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using Hospital_Management_Project.ViewModels;
 global using HospitalSystem.Application.Interfaces;
 global using HospitalSystem.Infrastructure.Services;
+global using HospitalSystem.Core.Entities;
 global using HospitalSystem.Persistance.Data;
 global using HospitalSystem.Persistance.UnitOfWork;
 global using Microsoft.AspNetCore.Identity;

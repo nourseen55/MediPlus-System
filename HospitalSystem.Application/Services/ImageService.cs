269ca63678302054e483eddf4df31463801e892d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-09-28 05:55:38 +0300

    Create a Service Interface and its Implementation for Image

---
+++ HospitalSystem.Application/Services/ImageService.cs
@@ -0,0 +1,40 @@
+﻿using HospitalSystem.Application.IServices;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem.Application.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> SaveImageAsync(IFormFile Img, string folderPath)
+        {
+            if (Img == null || Img.Length == 0)
+            {
+                return null;
+            }
+
+            string fileName = Guid.NewGuid().ToString();
+            string extension = Path.GetExtension(Img.FileName);
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, folderPath, fileName + extension);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await Img.CopyToAsync(fileStream);
+            }
+
+            return Path.Combine(folderPath, fileName + extension);
+        }
+    }
+}

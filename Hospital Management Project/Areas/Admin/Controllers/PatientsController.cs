d9683bf6757e42400547a96b1501c247020cf111
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-09-28 05:58:08 +0300

    Apply the Modifications on Controller

--- Hospital Management Project/Areas/Admin/Controllers/PatientsController.cs
+++ Hospital Management Project/Areas/Admin/Controllers/PatientsController.cs
@@ -1,4 +1,7 @@
-﻿using HospitalSystem.Application.Services;
+﻿
+using AutoMapper;
+using HospitalSystem.Application.IServices;
+using HospitalSystem.Application.Services;
 using HospitalSystem.Core.Entities;
 using HospitalSystem.Core.ViewModels;
 using Microsoft.AspNetCore.Hosting;
@@ -9,12 +12,14 @@ namespace Hospital_Management_Project.Areas.Patient.Controllers
     [Area("Admin")]
     public class PatientController : Controller
     {
+        private readonly IMapper _mapper;
         private readonly IPatientService _patientService;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        public PatientController(IPatientService patientService, IWebHostEnvironment webHostEnvironment)
+        private readonly IImageService _imageService;
+        public PatientController(IPatientService patientService, IImageService imageService, IMapper mapper)
         {
             _patientService = patientService;
-            _webHostEnvironment = webHostEnvironment;
+            _imageService = imageService;
+            _mapper = mapper;
         }
         public async Task<IActionResult> Index()
         {
@@ -44,23 +49,13 @@ namespace Hospital_Management_Project.Areas.Patient.Controllers
         {
             if (ModelState.IsValid)
             {
-                
-                string RootPath = _webHostEnvironment.WebRootPath;
-
                 if (Img != null && Img.Length > 0)
                 {
-                    string fileName = Guid.NewGuid().ToString();
-
-                    var filePath = Path.Combine(RootPath, @"Images\Patients");
-                    var extension = Path.GetExtension(Img.FileName);
-
-                    using (var fileStream = new FileStream(Path.Combine(filePath, fileName + extension), FileMode.Create))
-                    {
-                        await Img.CopyToAsync(fileStream);
-                    }
-
-                    patient.Img = @"Images\Patients\" + fileName + extension;
+                    var path = @"Images\Patients";
+                    string imgPath = await _imageService.SaveImageAsync(Img, path);
+                    patient.Img = imgPath;
                 }
+
                 await _patientService.AddPatientAsync(patient);
                 return RedirectToAction("Index");
             }
@@ -75,23 +70,7 @@ namespace Hospital_Management_Project.Areas.Patient.Controllers
             {
                 return NotFound();
             }
-            var model = new PatientVM
-            {
-                Id = Patient.Id,
-                FirstName = Patient.FirstName,
-                LastName = Patient.LastName,
-                Img = Patient.Img,
-                ZipCode = Patient.ZipCode,
-                City = Patient.City,
-                Country = Patient.Country,
-                Gender = Patient.Gender,
-                DateOfBirth = Patient.DateOfBirth,
-                Email = Patient.Email,
-                UserName = Patient.UserName,
-                PhoneNumber = Patient.PhoneNumber,
-                Password = Patient.PasswordHash
-            };
-
+            var model = _mapper.Map<PatientVM>(Patient);
             return View(model);
         }
 
@@ -101,41 +80,19 @@ namespace Hospital_Management_Project.Areas.Patient.Controllers
         {
             if (ModelState.IsValid)
             {
+                
                 var patient = await _patientService.GetPatientByIdAsync(patientVM.Id);
                 if (patient == null)
                 {
                     return NotFound();
                 }
-                patient.Id = patientVM.Id;
-                patient.FirstName = patientVM.FirstName;
-                patient.LastName = patientVM.LastName;
-                patient.ZipCode = patientVM.ZipCode;
-                patient.City = patientVM.City;
-                patient.Country = patientVM.Country;
-                patient.Gender = patientVM.Gender;
-                patient.DateOfBirth = patientVM.DateOfBirth;
-                patient.Email = patientVM.Email;
-                patient.UserName = patientVM.UserName;
-                patient.PhoneNumber = patientVM.PhoneNumber;
+                var model = _mapper.Map(patientVM, patient);
 
-
-
-
-                string RootPath = _webHostEnvironment.WebRootPath;
-
-                if (Img != null && Img.Length > 0)
+                if (Img != null && Img.Length > 0) 
                 {
-                    string fileName = Guid.NewGuid().ToString();
-
-                    var filePath = Path.Combine(RootPath, @"Images\Patients");
-                    var extension = Path.GetExtension(Img.FileName);
-
-                    using (var fileStream = new FileStream(Path.Combine(filePath, fileName + extension), FileMode.Create))
-                    {
-                        await Img.CopyToAsync(fileStream);
-                    }
-
-                    patient.Img = @"Images\Patients\" + fileName + extension;
+                    var path = @"Images\Patients";
+                    string imgPath = await _imageService.SaveImageAsync(Img, path);
+                    patient.Img = imgPath; 
                 }
                 await _patientService.UpdatePatientAsync(patient);
                 

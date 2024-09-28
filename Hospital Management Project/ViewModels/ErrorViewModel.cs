cb1bc98eb6f10853c46fd1a8103da7ba6b11f837
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-09-28 20:26:51 +0300

    Move ViewModel in hospitalsystem.Core

--- Hospital Management Project/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hospital_Management_Project.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

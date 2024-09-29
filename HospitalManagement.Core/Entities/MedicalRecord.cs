cd8ca7f5f8c75621c2a315b9de1f45882a834f82
rahma-mohmed <engrahma1422003@gmail.com>
2024-09-29 04:06:02 +0200

    Add Response attribute to medical record

--- HospitalManagement.Core/Entities/MedicalRecord.cs
+++ HospitalManagement.Core/Entities/MedicalRecord.cs
@@ -6,9 +6,10 @@
         public string Treatment { get; set; }
         public string Diagnosis { get; set; }
         public DateTime DateOfEntry { get; set; } = DateTime.Now;
-        public string PatientID { get; set; }
+        public string? Response { get; set; }
+        public string? PatientID { get; set; }
         public virtual Patient? Patient { get; set; }
-        public string DoctorID { get; set; }
+        public string? DoctorID { get; set; }
         public virtual Doctor? Doctor { get; set; }
     }
 }

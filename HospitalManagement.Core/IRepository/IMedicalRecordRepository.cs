33d55d5e04c389660e24fa78cd9e2bcbd18117bb
rahma-mohmed <engrahma1422003@gmail.com>
2024-09-29 04:06:41 +0200

    Add Services to Medical Record

---
+++ HospitalManagement.Core/IRepository/IMedicalRecordRepository.cs
@@ -0,0 +1,20 @@
+﻿using HospitalSystem.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem.Core.IRepository
+{
+    public interface IMedicalRecordRepository
+    {
+        Task AddEntityAsync(MedicalRecord entity);
+        Task<IEnumerable<MedicalRecord>> GetAllEntityAsync();
+        Task<MedicalRecord?> GetEntityByIdAsync(int id);
+        Task UpdateEntityAsync(MedicalRecord entity);
+        Task DeleteEntityAsync(int id);
+        Task<List<MedicalRecord>> GetMedicalRecordsByPatientIdAsync(string patientId);
+        Task<MedicalRecord> GetMedicalRecordAndPatientDetails(int id);
+    }
+}

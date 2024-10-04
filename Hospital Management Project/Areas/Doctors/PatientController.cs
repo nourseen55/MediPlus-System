b29142c24264cc97c1355d491c9d873639461738
nourseen <nourseen673@gmail.com>
2024-10-04 23:30:10 +0300

    View Specific Patient Of Doctor and Delete it

---
+++ Hospital Management Project/Areas/Doctors/PatientController.cs
@@ -0,0 +1,25 @@
+﻿using HospitalSystem.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_Management_Project.Areas.Doctors
+{
+    
+    public class PatientController : Controller
+    {
+        private readonly IAppointmentService _appointmentService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PatientController(IAppointmentService appointmentService, UserManager<ApplicationUser> userManager)
+        {
+            _appointmentService = appointmentService;
+            _userManager = userManager;
+            
+        }
+        public IActionResult Index()
+        {
+            var doctorid = _userManager.GetUserId(User);
+            var patients=_appointmentService.GetPatientsByDoctorAsync(doctorid);
+            return View(patients);
+        }
+    }
+}

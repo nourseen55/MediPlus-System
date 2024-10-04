b1b307b699d6f1a6694ffaad853e4e294fc82739
nourseen <nourseen673@gmail.com>
2024-10-04 22:12:50 +0300

    Add FirstName and LastName

--- HospitalManagement.Core/ViewModels/NurseVM.cs
+++ HospitalManagement.Core/ViewModels/NurseVM.cs
@@ -11,7 +11,8 @@ namespace HospitalSystem.Core.ViewModels
     public class NurseVM
     {
         public string? ID { get; set; } = Guid.NewGuid().ToString();
-        public string? Name { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? Img { get; set; }
         public string? ZipCode { get; set; }
         public string? City { get; set; }
@@ -24,8 +25,6 @@ namespace HospitalSystem.Core.ViewModels
         
         public string? UserName { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? Password { get; set; }
-
         public string? DoctorID { get; set; }
         public string? DepartmentID { get; set; }
         public  IEnumerable<Departments>? Departments { get; set; }

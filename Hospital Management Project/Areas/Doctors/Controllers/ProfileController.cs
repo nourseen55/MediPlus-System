fd0caf83c2434f806c02911da96ef23b9d293ac3
rahma-mohmed <engrahma1422003@gmail.com>
2024-10-07 03:58:26 +0200

    Add Doctor Profile in Doctor Area

---
+++ Hospital Management Project/Areas/Doctors/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using HospitalSystem.Core.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_Management_Project.Areas.Doctors.Controllers
+{
+    [Area("Doctors")]
+    [Authorize(Roles = nameof(UserRoles.Doctor))]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager) {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Doctor = await _userManager.GetUserAsync(User);
+            return View(Doctor);
+        }
+        //Edit , Add Profile
+    }
+}

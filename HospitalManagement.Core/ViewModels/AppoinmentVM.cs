5c817e008d80a4dca07ef364ffce68c127517be0
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-11 21:16:40 +0300

    Create appoinmentVM

---
+++ HospitalManagement.Core/ViewModels/AppoinmentVM.cs
@@ -0,0 +1,22 @@
+﻿using HospitalSystem.Core.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem.Core.ViewModels
+{
+    public class AppoinmentVM
+    {
+        public string PatientID { get; set; }
+        public string SelectedDepartmentID { get; set; }
+        public string SelectedDoctorID { get; set; }
+        public string SelectedWorkingHoursID { get; set; }
+
+        public IEnumerable<SelectListItem> Departments { get; set; }
+        public IEnumerable<SelectListItem> Doctors { get; set; }
+        public IEnumerable<SelectListItem> WorkingHours { get; set; }
+    }
+}

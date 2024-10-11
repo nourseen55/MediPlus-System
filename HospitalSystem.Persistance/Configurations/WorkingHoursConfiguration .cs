3d1a6e01ffa918e67cbd2e649e27d7db4f407274
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-11 21:16:02 +0300

    add workinghours configurations

---
+++ HospitalSystem.Persistance/Configurations/WorkingHoursConfiguration .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystem.Persistance.Configurations
+{
+    public class WorkingHoursConfiguration : IEntityTypeConfiguration<WorkingHours>
+    {
+        public void Configure(EntityTypeBuilder<WorkingHours> builder)
+        {
+
+            builder.ToTable("WorkingHours");
+                builder
+                .HasOne(w => w.Doctor) // Each WorkingHours has one Doctor
+                .WithMany(d => d.WorkingHours) // Doctor has many WorkingHours
+                .HasForeignKey(w => w.DoctorId) // Foreign key in WorkingHours
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}

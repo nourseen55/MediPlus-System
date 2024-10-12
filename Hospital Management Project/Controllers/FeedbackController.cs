d27e0473074f966f3a7ccab9c9962c75b93f706a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-12 21:00:15 +0300

    Create A FeedbackController

---
+++ Hospital Management Project/Controllers/FeedbackController.cs
@@ -0,0 +1,64 @@
+﻿using HospitalSystem.Application.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using X.PagedList;
+
+namespace Hospital_Management_Project.Controllers
+{
+    [Authorize]
+    public class FeedbackController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public FeedbackController(UserManager<ApplicationUser> userManager,ApplicationDbContext context )
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index(int? page)
+        {
+            int pageNum = page ?? 1;
+            int pageSize = 4;
+
+
+            var feedback =_context.feedbacks
+                .Include(n => n.ApplicationUser)
+                .OrderByDescending(n => n.DateFeedback)
+                .ToPagedList(pageNum, pageSize);
+
+            return View(feedback);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                feedback.UserId = _userManager.GetUserId(User);
+                feedback.DateFeedback = DateTime.Now;   
+
+
+                _context.feedbacks.Add(feedback);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmDelete(string id)
+        {
+            Feedback feedback = _context.feedbacks.FirstOrDefault(x => x.Id == id);
+
+            _context.feedbacks.Remove(feedback);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
